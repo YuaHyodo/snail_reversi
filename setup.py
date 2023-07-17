283ecea5bdbce8b787e8d5c4db06d9ce18510a62
YuaHyodo <66828980+YuaHyodo@users.noreply.github.com>
2023-07-18 00:04:16 +0900

    Add files via upload

--- setup.py
+++ setup.py
@@ -24,5 +24,5 @@ SOFTWARE.
 """
 from distutils.core import setup
 
-setup(name='snail_reversi', version='0.0.4', description='Library of reversi very very slow like a snail',
-         auther='Yua Hyodo', url='https://github.com/YuaHyodo/snail_reversi', packages=['snail_reversi'])
+setup(name='snail_reversi', version='0.0.5', description='Library of reversi very very slow like a snail',
+         author='Yua Hyodo', url='https://github.com/YuaHyodo/snail_reversi', packages=['snail_reversi'])
